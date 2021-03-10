b409f00801ff730363cc065004a607da7220eabf
marco <sun.dsk1@gmail.com>
2021-03-11 00:16:08 +0800

    upd config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -16,20 +16,20 @@ function getConfig (env) {
       return {
         networkId: 'mainnet',
         nodeUrl: 'https://rpc.mainnet.near.org',
-        contractName: CONTRACT_NAME,
-        walletUrl: 'https://wallet.near.org',
-        helperUrl: 'https://helper.mainnet.near.org',
-        explorerUrl: 'https://explorer.mainnet.near.org'
+        contractName: CONTRACT_NAME
+        // walletUrl: 'https://wallet.near.org',
+        // helperUrl: 'https://helper.mainnet.near.org',
+        // explorerUrl: 'https://explorer.mainnet.near.org'
       }
     case 'development':
     case 'testnet':
       return {
         networkId: 'testnet',
         nodeUrl: 'https://rpc.testnet.near.org',
-        contractName: CONTRACT_NAME,
-        walletUrl: 'https://wallet.testnet.near.org',
-        helperUrl: 'https://helper.testnet.near.org',
-        explorerUrl: 'https://explorer.testnet.near.org'
+        contractName: CONTRACT_NAME
+        // walletUrl: 'https://wallet.testnet.near.org',
+        // helperUrl: 'https://helper.testnet.near.org',
+        // explorerUrl: 'https://explorer.testnet.near.org'
       }
     default:
       throw Error(`Unconfigured environment '${env}'. Can be configured in src/config.js.`)
