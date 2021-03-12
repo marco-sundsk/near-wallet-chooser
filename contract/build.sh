eb6a695cb7e6e04f1225c97921bd24f908ac9d43
marco <sun.dsk1@gmail.com>
2021-03-12 14:50:58 +0800

    contract ready

--- contract/build.sh
+++ contract/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
 
-RUSTFLAGS='-C link-arg=-s' cargo +stable build --target wasm32-unknown-unknown --release
-cp target/wasm32-unknown-unknown/release/wallet_registry.wasm ./res/
-
+RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
+cp target/wasm32-unknown-unknown/release/selector.wasm ./res/
